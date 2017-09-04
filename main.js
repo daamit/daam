68a7684d52cd6bbba8c1df532cd1a875568c7c8c
daam <daam@daam.it>
2017-09-04 17:31:54 +0000

    V1JS

---
+++ main.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+
+    // Blocks
+
+    // - Decrypt and show email
+    // - Auto resize text areas
+
+    //============================================= Decrypt and show email =============================================
+
+    var var_1, var_2, var_3, var_4;
+
+    var_1 = 'da';
+    var_2 = 'am@';
+    var_3 = 'daa';
+    var_4 = 'm.it';
+
+    $('.mail-btn-js').on('click', function(e) {
+        e.preventDefault();
+        $('.email-js').text(var_1 + var_2 + var_3 + var_4).css({'display' : 'inline-block'});
+        $(this).hide();
+    });
+
+    //============================================= /# Decrypt and show email ==========================================
+
+});
