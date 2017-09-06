bbf65f208add32073a38a72bd4b6db9db8cdb2d2
daam <daam@daam.it>
2017-09-06 19:50:18 +0000

    chartjs

--- main.js
+++ main.js
@@ -29,42 +29,24 @@ jQuery(document).ready(function($) {
      * Chart.js
      */
 
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
-    });
+     var ctx = document.getElementById('myChart').getContext('2d');
+ var chart = new Chart(ctx, {
+     // The type of chart we want to create
+     type: 'line',
+
+     // The data for our dataset
+     data: {
+         labels: ["March", "April", "May", "June", "July", "August", "September"],
+         datasets: [{
+             label: "Instagram Followers",
+             backgroundColor: 'rgb(255, 99, 132)',
+             borderColor: 'rgb(255, 99, 132)',
+             data: [3800, 4340, 5250, 6230, 7060, 7660, 8130],
+         }]
+     },
+
+     // Configuration options go here
+     options: {}
+ });
 
 });
